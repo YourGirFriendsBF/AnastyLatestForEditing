641ebaab47288f66ee4a2e30eefb285cb5496574
YourGirFriendsBF <109566119+YourGirFriendsBF@users.noreply.github.com>
2022-08-24 20:39:01 +0530

    Update listener.py

--- bot/modules/listener.py
+++ bot/modules/listener.py
@@ -5,7 +5,7 @@ from os import path as ospath, remove as osremove, listdir, walk
 from subprocess import Popen
 from html import escape
 
-from bot import Interval, INDEX_URL, BUTTON_FOUR_NAME, BUTTON_FOUR_URL, BUTTON_FIVE_NAME, BUTTON_FIVE_URL, \
+from bot import bot, Interval, INDEX_URL, BUTTON_FOUR_NAME, BUTTON_FOUR_URL, BUTTON_FIVE_NAME, BUTTON_FIVE_URL, \
                 BUTTON_SIX_NAME, BUTTON_SIX_URL, VIEW_LINK, aria2, DOWNLOAD_DIR, download_dict, download_dict_lock, \
                 TG_SPLIT_SIZE, LOGGER, DB_URI, INCOMPLETE_TASK_NOTIFIER, LEECH_LOG, BOT_PM, MIRROR_LOGS, LINK_LOGS, \
                 SOURCE_LINK
